ae6c8232c06d6cf67b6531312985074750c63827
JonatasGomes <jonatasgomes1090@gmail.com>
2025-05-27 20:13:17 -0300

    style: update style image

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
-<template >
-  <v-container fluid class="fundo">
-    <tela-login/>
+<template>
+  <v-container fluid class="fundo-container">
+    <div class="fundo"></div>
+    <tela-login />
   </v-container>
 </template>
 
-<script setup>
-  
-</script>
+<script setup></script>
 
 <style>
-.fundo{
+.fundo {
   background-color: aqua;
-  height: 100vh;
+  min-height: 100vh;
+  height: 100%;
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
-  background-image: url('https://images.pexels.com/photos/32058694/pexels-photo-32058694.jpeg');
-  position: relative; 
-  z-index: 0;}
+  /* Add the blur effect */
+  filter: blur(8px);
+
+  background-image: url("https://images.pexels.com/photos/32058694/pexels-photo-32058694.jpeg");
+  position: relative;
+  z-index: 0;
+}
+.fundo-container {
+  overflow: hidden;
+  margin: 0px;
+  padding: 0px;
+  width: 100vw;
+  height: 100vh;
+}
 </style>
